a19a3c9f5691867f4f3663e9ea64a57d02f15730
laouernour <imene2003.in@gmail.com>
2024-04-29 17:55:10 +0200

    formulaire médecin

--- authentification.py
+++ authentification.py
@@ -8,29 +8,29 @@ class Personne():
         self.formulaire_frame = ct.CTkFrame(master,  fg_color='#FFFFFF', border_width=2, border_color='#263A5F')
         self.formulaire_frame.grid()
 
-        self.nom_label = ct.CTkLabel(self.formulaire_frame, text="Nom :", font=('Karla', 20))
+        self.nom_label = ct.CTkLabel(self.formulaire_frame, text="Nom :", font=('Karla', 18))
         self.nom_label.grid(row=0, column=0, padx=20, pady=20, sticky="w")
-        self.nom_entry = ct.CTkEntry(self.formulaire_frame, width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.nom_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
         self.nom_entry.grid(row=0, column=1, padx=20, pady=20, sticky="w")
 
-        self.prenom_label = ct.CTkLabel(self.formulaire_frame, text="Prénom :", font=('Karla', 20))
+        self.prenom_label = ct.CTkLabel(self.formulaire_frame, text="Prénom :", font=('Karla', 18))
         self.prenom_label.grid(row=1, column=0, padx=20, pady=20, sticky="w")
-        self.prenom_entry = ct.CTkEntry(self.formulaire_frame, width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.prenom_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
         self.prenom_entry.grid(row=1, column=1, padx=20, pady=20, sticky="w")
 
-        self.date_naissance_label = ct.CTkLabel(self.formulaire_frame, text="Date de Naissance :", font=('Karla', 20))
+        self.date_naissance_label = ct.CTkLabel(self.formulaire_frame, text="Date de Naissance :", font=('Karla', 18))
         self.date_naissance_label.grid(row=2, column=0, padx=20, pady=20,sticky="w")
-        self.date_naissance_entry = ct.CTkEntry(self.formulaire_frame, width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.date_naissance_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
         self.date_naissance_entry.grid(row=2, column=1, padx=20, pady=20, sticky="w")
 
-        self.wilaya_label = ct.CTkLabel(self.formulaire_frame, text="Wilaya :", font=('Karla', 20))
+        self.wilaya_label = ct.CTkLabel(self.formulaire_frame, text="Wilaya :", font=('Karla', 18))
         self.wilaya_label.grid(row=3, column=0, padx=20, pady=20, sticky="w")
-        self.wilaya_entry = ct.CTkEntry(self.formulaire_frame, width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.wilaya_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
         self.wilaya_entry.grid(row=3, column=1, padx=20, pady=20, sticky="w")
 
-        self.phone_nmbr_label = ct.CTkLabel(self.formulaire_frame, text="Téléphone:", font=('Karla', 20))
+        self.phone_nmbr_label = ct.CTkLabel(self.formulaire_frame, text="Téléphone:", font=('Karla', 18))
         self.phone_nmbr_label.grid(row=4, column=0, padx=20, pady=20, sticky="w")
-        self.phone_nmbr_entry = ct.CTkEntry(self.formulaire_frame, width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.phone_nmbr_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
         self.phone_nmbr_entry.grid(row=4, column=1, padx=20, pady=20, sticky="w")
 
 class Inscrire(ct.CTk):
@@ -41,28 +41,28 @@ class Inscrire(ct.CTk):
         self.geometry("%dx%d+0+0" % (w, h))
         self.configure(bg='#263A5F')
 
-        self.formulaire_frame1 = ct.CTkFrame(self, width=500, height=500, fg_color='#FFFFFF', border_width=2, border_color='#263A5F', corner_radius=10)
-        self.formulaire_frame1.pack(expand=True)
+        self.formulaire_frame1 = ct.CTkFrame(self,width=600,height=750,  fg_color='#FFFFFF', border_width=2, border_color='#263A5F', corner_radius=10)
+        self.formulaire_frame1.pack()
 
         self.personne = Personne(self.formulaire_frame1)
 
-        self.matricule_ADM_label = ct.CTkLabel(self.formulaire_frame1, text="Matricule :", font=('Karla', 20))
+        self.matricule_ADM_label = ct.CTkLabel(self.formulaire_frame1, text="Matricule :", font=('Karla', 18))
         self.matricule_ADM_label.grid(row=5, column=0, padx=20, pady=20,sticky="w")
-        self.matricule_ADM_entry = ct.CTkEntry(self.formulaire_frame1, width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.matricule_ADM_entry = ct.CTkEntry(self.formulaire_frame1, width=250, height=35, corner_radius=10, font=('Karla', 14))
         self.matricule_ADM_entry.grid(row=5, column=1, padx=20, pady=20, sticky="w")
 
-        self.MP_label = ct.CTkLabel(self.formulaire_frame1, text="Mot de passe :", font=('Karla', 20))
+        self.MP_label = ct.CTkLabel(self.formulaire_frame1, text="Mot de passe :", font=('Karla', 18))
         self.MP_label.grid(row=6, column=0, padx=20, pady=20, sticky="w")
-        self.MP_entry = ct.CTkEntry(self.formulaire_frame1, show="*", width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.MP_entry = ct.CTkEntry(self.formulaire_frame1, show="*", width=250, height=35, corner_radius=10, font=('Karla', 14))
         self.MP_entry.grid(row=6, column=1, padx=20, pady=20, sticky="w")
 
-        self.confirmation_MP_label = ct.CTkLabel(self.formulaire_frame1, text="Confirmation du mot de passe :", font=('Karla', 20))
+        self.confirmation_MP_label = ct.CTkLabel(self.formulaire_frame1, text="Confirmation du mot de passe :", font=('Karla', 18))
         self.confirmation_MP_label.grid(row=7, column=0, padx=20, pady=20, sticky="w")
-        self.confirmation_MP_entry = ct.CTkEntry(self.formulaire_frame1,show="*", width=300, height=40, corner_radius=10, font=('Karla', 16))
+        self.confirmation_MP_entry = ct.CTkEntry(self.formulaire_frame1,show="*", width=250, height=35, corner_radius=10, font=('Karla', 16))
         self.confirmation_MP_entry.grid(row=7, column=1, padx=20, pady=20, sticky="w")
 
-        self.enregistrer_ADM= ct.CTkButton(self.formulaire_frame1, text="Créer", command=self.creer, width=150, height=40, corner_radius=15, font=('Karla', 20, 'bold'), fg_color='#263A5F', cursor='hand2', text_color='#FFFFFF')
-        self.enregistrer_ADM.grid(row=7, columnspan=2, pady=20)
+        self.enregistrer_ADM= ct.CTkButton(self.formulaire_frame1, text="Créer", command=self.creer, width=200, height=30, corner_radius=15, font=('Karla', 18, 'bold'), fg_color='#263A5F', cursor='hand2', text_color='#FFFFFF')
+        self.enregistrer_ADM.grid(row=8, column=0, columnspan=2, pady=10)
 
     def creer(self):
         if self.personne.nom_entry.get() == "" or self.personne.prenom_entry.get() == "" or self.personne.date_naissance_entry.get() == "" or self.personne.wilaya_entry.get() == "" or self.personne.phone_nmbr_entry.get() == "" or self.matricule_ADM_entry.get() == "" or self.MP_entry.get() == "" or self.confirmation_MP_entry.get() == "":
---
+++ doctor.py
@@ -0,0 +1,72 @@
+import tkinter as tk
+import customtkinter as ct
+from tkinter import ttk
+
+
+class Personne():
+    def __init__(self, master):
+        self.master = master
+        self.formulaire_frame = ct.CTkFrame(master,  fg_color='#FFFFFF', border_width=2, border_color='#263A5F')
+        self.formulaire_frame.grid()
+
+        self.nom_label = ct.CTkLabel(self.formulaire_frame, text="Nom :", font=('Karla', 18))
+        self.nom_label.grid(row=0, column=0, padx=20, pady=20, sticky="w")
+        self.nom_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
+        self.nom_entry.grid(row=0, column=1, padx=20, pady=20, sticky="w")
+
+        self.prenom_label = ct.CTkLabel(self.formulaire_frame, text="Prénom :", font=('Karla', 18))
+        self.prenom_label.grid(row=1, column=0, padx=20, pady=20, sticky="w")
+        self.prenom_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
+        self.prenom_entry.grid(row=1, column=1, padx=20, pady=20, sticky="w")
+
+        self.date_naissance_label = ct.CTkLabel(self.formulaire_frame, text="Date de Naissance :", font=('Karla', 18))
+        self.date_naissance_label.grid(row=2, column=0, padx=20, pady=20,sticky="w")
+        self.date_naissance_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
+        self.date_naissance_entry.grid(row=2, column=1, padx=20, pady=20, sticky="w")
+
+        self.wilaya_label = ct.CTkLabel(self.formulaire_frame, text="Wilaya :", font=('Karla', 18))
+        self.wilaya_label.grid(row=3, column=0, padx=20, pady=20, sticky="w")
+        self.wilaya_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
+        self.wilaya_entry.grid(row=3, column=1, padx=20, pady=20, sticky="w")
+
+        self.phone_nmbr_label = ct.CTkLabel(self.formulaire_frame, text="Téléphone:", font=('Karla', 18))
+        self.phone_nmbr_label.grid(row=4, column=0, padx=20, pady=20, sticky="w")
+        self.phone_nmbr_entry = ct.CTkEntry(self.formulaire_frame, width=250, height=35, corner_radius=10, font=('Karla', 14))
+        self.phone_nmbr_entry.grid(row=4, column=1, padx=20, pady=20, sticky="w")
+
+        # Add other fields similarly
+
+class Inscrire_doctor(ct.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("S'inscrire")
+        w, h = self.winfo_screenwidth(), self.winfo_screenheight()
+        self.geometry("%dx%d+0+0" % (w, h))
+        self.configure(bg='#263A5F')
+
+        self.formulaire_frame2 = ct.CTkFrame(self,width=600,height=750,  fg_color='#FFFFFF', border_width=2, border_color='#263A5F', corner_radius=10)
+        self.formulaire_frame2.pack()
+
+        self.personne = Personne(self.formulaire_frame2)
+
+        self.matricule_doctor_label = ct.CTkLabel(self.formulaire_frame2, text="Matricule Médecin:", font=('Karla', 18))
+        self.matricule_doctor_label.grid(row=5, column=0, padx=20, pady=20,sticky="w")
+        self.matricule_doctor_entry = ct.CTkEntry(self.formulaire_frame2, width=250, height=35, corner_radius=10, font=('Karla', 14))
+        self.matricule_doctor_entry.grid(row=5, column=1, padx=20, pady=20, sticky="w")
+
+        self.grad_label = ct.CTkLabel(self.formulaire_frame2, text="Grad:", font=('Karla', 18))
+        self.grad_label.grid(row=6, column=0, padx=20, pady=20, sticky="w")
+
+        grad = ["Généraliste", "Résident", "Assistant", "Maître Assistant", "Professeur"]
+        self.grad_entry = ct.CTkComboBox(self.formulaire_frame2, values=grad,dropdown_font= ('Karla', 14),width=250, height=35, corner_radius=10,
+                                         font=('Karla', 14),dropdown_fg_color='#FFFFFF')
+        self.grad_entry.grid(row=6, column=1, padx=20, pady=20, sticky="w")
+
+        self.enregistrer_doctor = ct.CTkButton(self.formulaire_frame2, text="Créer", command=None, width=150,
+                                            height=40, corner_radius=15, font=('Karla', 18, 'bold'), fg_color='#263A5F',
+                                            cursor='hand2', text_color='#FFFFFF')
+        self.enregistrer_doctor.grid(row=7, column=0, columnspan=2, pady=10)
+
+# Create an instance of the custom window class and start the main loop
+app = Inscrire_doctor()
+app.mainloop()
